e139fcde838c33133eb05377f13f019bddd27e69
adarshzpatel <adarshzpatel@gmail.com>
2024-01-11 15:15:07 +0530

    reset stores & solana fns

--- src/hooks/useSolBalance.tsx
+++ src/hooks/useSolBalance.tsx
@@ -2,24 +2,23 @@ import { useAnchorWallet, useConnection } from "@solana/wallet-adapter-react";
 import { useEffect, useState } from "react";
 
 const useSolBalance = () => {
-  const [solBalance, setSolBalance] = useState<number|null>(null);
+  const [solBalance, setSolBalance] = useState<number | null>(null);
   const { connection } = useConnection();
   const connectedWallet = useAnchorWallet();
 
   const getSolBalance = async () => {
-    try{
-
-      if(!connectedWallet) {
+    try {
+      if (!connectedWallet) {
         setSolBalance(null);
-        return
-      };
+        return;
+      }
       const bal = await connection.getBalance(connectedWallet?.publicKey);
-      setSolBalance(bal);
-      
-    } catch(err){
-      console.log("Err in getSolBalance",err);
+      setSolBalance(bal ?? 0.0);
+    } catch (err) {
+      console.log("Err in getSolBalance", err);
     }
   };
+
   useEffect(() => {
     getSolBalance();
   }, [connection, connectedWallet]);
